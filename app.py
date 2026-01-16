07d5ebf6405b1189a925af4c4bbedb0fe2dfd7c2
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2026-01-16 16:33:02 +0530

    subtle fixes

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ print('------------------------------------')
 yt_audio = {
     'format': 'bestaudio[ext=m4a]/bestaudio[ext=mp3]/bestaudio[ext=wav]/bestaudio/best',
     'ignore_expires': True,
+    'outtmpl': 'audio/%(title)s.%(ext)s',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
@@ -28,18 +29,16 @@ with dlp.YoutubeDL(yt_audio) as ydl:
     duration = videoInfo['duration'] # retreive video duration in seconds
     ydl.download([audioLink])
 
-audioFile = f'{videoInfo['fulltitle']} [{videoInfo['display_id']}].mp3' 
 folder = videoInfo['fulltitle']
 os.mkdir(f'chunkAudio/{folder}') # create folder for chunks
 
 # pydub section
-file = AudioSegment.from_mp3(audioFile)
+file = AudioSegment.from_mp3(f'audio/{videoInfo['fulltitle']}.mp3')
 count = 0 # variable for output filename iteration tracking
 
 for splitStart in range(0, int(duration), 30):
     for splitStop in range(splitStart+30, splitStart+60, 30):
         # Example: 0-30, 30-60, 60-90, etc...
-        print(splitStart,splitStop)
         count += 1
 
         chunk = file[splitStart*1000:splitStop*1000] # split in 30 second chunks
