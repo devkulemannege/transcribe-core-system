7682296f4d36b4fd30cc2575bbb4df610291a252
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2026-01-17 22:18:01 +0530

    cleanup code

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from pydub import AudioSegment
 import yt_dlp as dlp
-from flask import Flask, jsonify
-import shutil
 import requests
-import time
+import shutil
 import os
 import re
 
-# TODO: change later
+''' # Remove the comment markers to automatically delete previous downloaded audio and it's chunks
 try:
     shutil.rmtree('audio')
     os.mkdir('audio')
@@ -15,30 +13,23 @@ try:
     shutil.rmtree('chunkAudio')
     os.mkdir('chunkAudio')
 except Exception as e:
-    print(f'{e}')
+    print(f'{e}')'''
 
 # create dir & remove transcription.txt for fresh transcription
-try:
-    os.mkdir('chunkAudio')
-except Exception as e:
-    print(e)
-
-try:
-    os.mkdir('audio')
-except Exception as e:
-    print(e)
-
-try:
-    os.remove('transcription.txt')
-except Exception as e:
-    print(e)
+try:os.mkdir('chunkAudio')
+except Exception as e:print(e)
+try:os.mkdir('audio')
+except Exception as e:print(e)
+try:os.remove('transcription.txt')
+except Exception as e:print(e)
 
 # get input file path
 print('------------------------------------')
-audioLink = input('Enter link: ')
-server_url = input('Enter url: ')
+audioLink = input('Enter video link: ')
+server_url = input('Enter server url: ')
 print('------------------------------------')
 
+# yt-dlp section
 yt_audio = {
     'format': 'bestaudio[ext=m4a]/bestaudio[ext=mp3]/bestaudio[ext=wav]/bestaudio/best',
     'ignore_expires': True,
@@ -60,21 +51,22 @@ folder = videoInfo['fulltitle']
 os.mkdir(f'chunkAudio/{folder}') # create folder for chunks
 
 # pydub section
-file = AudioSegment.from_mp3(f'audio/{videoInfo['fulltitle']}.mp3')
-count = 0 # variable for output filename iteration tracking
+file = AudioSegment.from_mp3(f'audio/{folder}.mp3')
+count = 0 # variable for output filename iteration tracking (Ex: chunk1, chunk2, chunk3)
 
 for splitStart in range(0, int(duration), 5):
     for splitStop in range(splitStart+5, splitStart+10, 5):
         # Example: 0-5, 5-10, 10-15, etc...
         count += 1
 
-        chunk = file[splitStart*1000:splitStop*1000] # split in 30 second chunks
+        chunk = file[splitStart*1000:splitStop*1000] # split in 5 second chunks
         chunk.export(f'chunkAudio/{folder}/chunk{count}.mp3',format='mp3')
 
 chunkDir = os.listdir(f'chunkAudio/{folder}') # get chunk files as a list
 chunkDir.sort(key=lambda var: [int(x) if x.isdigit() else x for x in re.split('([0-9]+)', var)])
-print(f'Chunk Files: {chunkDir}')
+print(f'Chunk Files: {chunkDir}') # debug
 
+# server transaction section
 try:
     for chunk in chunkDir:
         with open(f'chunkAudio/{folder}/{chunk}', 'rb') as binary: # read chunk files as binary and send to server
@@ -83,7 +75,8 @@ try:
             response = requests.post(
                 server_url, 
                 files= {'file': (chunk, binary, 'audio/mpeg')},
-                timeout=600).json()
+                timeout=600
+            ).json()
 
             with open('transcription.txt','a', encoding='utf-8') as export:
                 export.write(response['reply']) # write to transcription.txt
@@ -91,4 +84,6 @@ try:
         print(f'Transcription finished for {chunk}')
 except Exception as e:
     print(f'Failed to communicate with server: {e}')
+
+print('\nProcess Finished.')
     
